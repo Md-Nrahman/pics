f5cafcd01dbcf00aa938083677759758f8134757
md-nrahman <md.naimur.rahman.naim@gmail.com>
2020-04-08 16:57:39 +0600

    Updates

---
+++ precache-manifest.c9dc18c3260d8dee49d5f2b90ef0d591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe437ef6e7ce03829dcec83bc8b6b22",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "1527f4bca1386a926759",
+    "url": "/pics/static/css/main.11d177d3.chunk.css"
+  },
+  {
+    "revision": "45a4f69279a0fdf8870b",
+    "url": "/pics/static/js/2.dc21667d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.dc21667d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1527f4bca1386a926759",
+    "url": "/pics/static/js/main.bc5368f8.chunk.js"
+  },
+  {
+    "revision": "c5a0de5d9d3a9d267419",
+    "url": "/pics/static/js/runtime-main.d04fa802.js"
+  }
+]);
